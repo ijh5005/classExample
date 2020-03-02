4970b1de9b2e7bc3d12108f25943f8fa66dfb6dc
Isaiah Harrison <ijh5005@outlook.com>
2020-03-02 18:53:50 -0500

    front end added

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hey")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, console.log(`app listening on port ${PORT}`));
